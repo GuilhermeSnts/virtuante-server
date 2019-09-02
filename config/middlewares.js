11d63106d641b79b5329f1202687c1f5539e0052
Guilherme Santos <guilhermess1994@gmail.com>
2019-09-02 01:48:49 -0300

    cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -4,12 +4,13 @@ const logger = require('morgan')
 
 module.exports = app => {
     
-    app.use(cors()) //Cross Origin Resource Sharing
+     //Cross Origin Resource Sharing
     app.use(bodyParser.json({
         limit: "5mb",
         extended: true,
         parameterLimit: 50000
     }))
     app.use(logger('tiny'))
+    app.use(cors())
     
 }
